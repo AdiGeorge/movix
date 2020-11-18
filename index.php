2592dc0a4948fa8c4dbeb53a30c9e1d46e629e01
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2020-11-18 09:38:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movix</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+   <?php require_once(__DIR__.'/components/left/left-main.html') ?>
+    <?php require_once(__DIR__.'/components/middle/middle.html') ?>
+  <?php require_once(__DIR__.'/components/right/right.html') ?>  
+   
+   <script src="app.js"></script>
+</body>
+</html>
