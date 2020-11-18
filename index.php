d910577f69f0f359bc6965c5db92f0f0cc8940be
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2020-11-18 21:20:57 +0100

    dashboard movies

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
     <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
+  
    <?php require_once(__DIR__.'/components/left/left-main.html') ?>
     <?php require_once(__DIR__.'/components/middle/middle.html') ?>
   <?php require_once(__DIR__.'/components/right/right.html') ?>  
-   
+    
+  
+  <script src="swiper.min.js"></script>
+
    <script src="app.js"></script>
 </body>
 </html>
