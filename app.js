51cfc7a842405e408d870ae0dcd8c26393af6468
Ioana Andreea Mihai <ioan0579@stud.kea.dk>
2020-11-20 20:40:15 +0100

    movie modal

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 
 var acc = document.getElementsByClassName("categ-title-arrow");
-
+var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     var panel = this.nextElementSibling;
@@ -20,4 +20,31 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movix</title>
     <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 <body>
   
@@ -15,8 +16,7 @@
     
   
   <script src="swiper.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ 
    <script src="app.js"></script>
 </body>
 </html>
