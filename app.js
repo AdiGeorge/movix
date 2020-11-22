2348444e67847de49baf059031cc36ecf4b8f8e4
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2020-11-22 16:23:41 +0100

    asdfgh

---
+++ app.js
@@ -0,0 +1,54 @@
+// 'use strict'
+
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import 'swiper/swiper-bundle.css';
+
+// const swiper = new Swiper(...);
+alert();
+function showPage(pageId){
+  document.querySelectorAll('.subpage').forEach( item => {
+      item.style.display = "none"
+  })
+  document.getElementById(pageId).style.display="block"
+}
+
+var acc = document.getElementsByClassName("categ-title-arrow");
+
+for (var i=0; i<acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    } 
+  });
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
