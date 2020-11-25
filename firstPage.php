edc4bd8a682164718ccc0feca400266213bc0162
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2020-11-25 19:23:50 +0100

    movie page and profile

--- firstPage.php
+++ firstPage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movix</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
   
@@ -14,8 +14,8 @@
 
     
   
-  <script src="swiper.min.js"></script>
+  <!-- <script src="swiper.min.js"></script> -->
 
-   <script src="app.js"></script>
+   <script src="./js/app.js"></script>
 </body>
 </html>
